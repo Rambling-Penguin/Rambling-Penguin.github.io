77ef0ec6c391ef96d14ab0d9ef7c10d53f1ecad6
Ubuntu-dev <dan@codecastle.io>
2021-04-07 21:45:17 -0700

    Added Sport of kings

--- albums/albums.js
+++ albums/albums.js
@@ -1,5 +1,7 @@
 import {Nature} from "./nature/album.js";
+import {SPORT_OF_KINGS_2019} from "./2019-Sport-of-Kings/album.js";
 
 export const AllAlbums = [
-    Nature
+    Nature,
+    SPORT_OF_KINGS_2019
 ];
