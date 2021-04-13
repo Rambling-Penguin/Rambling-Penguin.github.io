6acb293c1004389b1f6f2b747b9a1a9977d8564e
Dan Antal <daniel.antal@nike.com>
2021-04-13 15:46:38 -0700

    Added 2019 twelfth night

--- albums/albums.js
+++ albums/albums.js
@@ -4,10 +4,12 @@ import {Nature} from "./nature.js";
 import {SPORT_OF_KINGS_2019} from "./2019-Sport-of-Kings.js";
 import {SEPTEMBER_CROWN_2019} from "./2019-September-Crown.js";
 import {AN_TIR_WEST_2019} from "./2019-An-Tir-West.js";
+import {AN_TIR_TWELFTH_NIGHT_2019} from "./2019-Twelfth-Night.js";
 
 export const AllAlbums = [
     Nature,
     SPORT_OF_KINGS_2019,
     SEPTEMBER_CROWN_2019,
-    AN_TIR_WEST_2019
+    AN_TIR_WEST_2019,
+    AN_TIR_TWELFTH_NIGHT_2019
 ];
