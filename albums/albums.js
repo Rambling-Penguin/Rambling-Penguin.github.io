eeea100b7f56f5780566db3b9bdc1b32c8dde401
Dan Antal <daniel.antal@nike.com>
2021-04-15 13:24:42 -0700

    Added carnevale 2019

--- albums/albums.js
+++ albums/albums.js
@@ -5,11 +5,13 @@ import {SPORT_OF_KINGS_2019} from "./2019-Sport-of-Kings.js";
 import {SEPTEMBER_CROWN_2019} from "./2019-September-Crown.js";
 import {AN_TIR_WEST_2019} from "./2019-An-Tir-West.js";
 import {AN_TIR_TWELFTH_NIGHT_2019} from "./2019-Twelfth-Night.js";
+import {CARNEVALE_2019} from "./2019-Carnevale.js";
 
 export const AllAlbums = [
     Nature,
     SPORT_OF_KINGS_2019,
     SEPTEMBER_CROWN_2019,
     AN_TIR_WEST_2019,
-    AN_TIR_TWELFTH_NIGHT_2019
+    AN_TIR_TWELFTH_NIGHT_2019,
+    CARNEVALE_2019
 ];
--- js/subscribeButton.js
+++ js/subscribeButton.js
@@ -44,17 +44,10 @@ export default class SubscribeButton extends HTMLElement {
                                                 <label for="mce-LNAME">Last Name </label>
                                                 <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
                                             </div>
-                                            <div class="mc-field-group size1of2">
-                                                <label for="mce-BIRTHDAY-month">Birthday </label>
-                                                <div class="datefield">
-                                                    <span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="BIRTHDAY[month]" id="mce-BIRTHDAY-month"></span> /
-                                                    <span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="BIRTHDAY[day]" id="mce-BIRTHDAY-day"></span>
-                                                    <span class="small-meta nowrap">( mm / dd )</span>
-                                                </div>
-                                            </div>	<div id="mce-responses" class="clear">
-                                            <div class="response" id="mce-error-response" style="display:none"></div>
-                                            <div class="response" id="mce-success-response" style="display:none"></div>
-                                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                           	<div id="mce-responses" class="clear">
+                                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                                <div class="response" id="mce-success-response" style="display:none"></div>
+                                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                                             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5e4c26fc70e4ec138aaa0a9aa_c505fdb0fb" tabindex="-1" value=""></div>
                                             <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                                         </div>
@@ -66,6 +59,10 @@ export default class SubscribeButton extends HTMLElement {
                     </div>
                 </div>
             </div>`;
+
+        this.querySelector('form[name="mc-embedded-subscribe-form"]').addEventListener('submit', () => {
+            ga('send', 'event', 'Subscriber', 'subscribed');
+        });
     }
 
 }
